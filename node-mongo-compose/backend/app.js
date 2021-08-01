846cb0d3e2363c7f8ddac3c218db658e4a84431e
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-08-01 12:59:33 -0300

    [feat]: add page main html with commands for register a useradd main html page with commands for register a user

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const restful = require('node-restful');
-const server = express();
-const mongoose = restful.mongoose;
-
-const PORT = 3000;
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // Database
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://db/mydb');
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
 
-// Test
-server.get('/', (req, res, next) => res.send('Backend'));
+// Middlewares
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+server.use(cors())
 
-// Start server
-server.listen(PORT, () => console.log("Server running on port " + PORT));
+// ODM
+const Client = restful.model('Client', {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+// Routes
+Client.register(server, '/clients')
+
+// Start Server
+server.listen(3000)
